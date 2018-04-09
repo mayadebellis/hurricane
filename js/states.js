42e6e0c9aa0ae7d38216ae3bafeda1020da0dd88
olivaceously <olivia.macdougal@tufts.edu>
2018-04-09 12:48:33 -0400

    deleted extraneous files, single state colored

---
+++ js/states.js
@@ -0,0 +1,258 @@
+var states_json = 
+[
+  {
+    "name": "Alabama",
+    "sid": "01",
+    "abbrev": "AL"
+  },
+  {
+    "name": "Alaska",
+    "sid": "02",
+    "abbrev": "AK"
+  },
+  {
+    "name": "Arizona",
+    "sid": "04",
+    "abbrev": "AZ"
+  },
+  {
+    "name": "Arkansas",
+    "sid": "05",
+    "abbrev": "AR"
+  },
+  {
+    "name": "California",
+    "sid": "06",
+    "abbrev": "CA"
+  },
+  {
+    "name": "Colorado",
+    "sid": "08",
+    "abbrev": "CO"
+  },
+  {
+    "name": "Connecticut",
+    "sid": "09",
+    "abbrev": "CT"
+  },
+  {
+    "name": "Delaware",
+    "sid": "10",
+    "abbrev": "DE"
+  },
+  {
+    "name": "District of Columbia",
+    "sid": "11",
+    "abbrev": "DC"
+  },
+  {
+    "name": "Florida",
+    "sid": "12",
+    "abbrev": "FL"
+  },
+  {
+    "name": "Georgia",
+    "sid": "13",
+    "abbrev": "GA"
+  },
+  {
+    "name": "Hawaii",
+    "sid": "15",
+    "abbrev": "HI"
+  },
+  {
+    "name": "Idaho",
+    "sid": "16",
+    "abbrev": "ID"
+  },
+  {
+    "name": "Illinois",
+    "sid": "17",
+    "abbrev": "IL"
+  },
+  {
+    "name": "Indiana",
+    "sid": "18",
+    "abbrev": "IN"
+  },
+  {
+    "name": "Iowa",
+    "sid": "19",
+    "abbrev": "IA"
+  },
+  {
+    "name": "Kansas",
+    "sid": "20",
+    "abbrev": "KS"
+  },
+  {
+    "name": "Kentucky",
+    "sid": "21",
+    "abbrev": "KY"
+  },
+  {
+    "name": "Louisiana",
+    "sid": "22",
+    "abbrev": "LA"
+  },
+  {
+    "name": "Maine",
+    "sid": "23",
+    "abbrev": "ME"
+  },
+  {
+    "name": "Maryland",
+    "sid": "24",
+    "abbrev": "MD"
+  },
+  {
+    "name": "Massachusetts",
+    "sid": "25",
+    "abbrev": "MA"
+  },
+  {
+    "name": "Michigan",
+    "sid": "26",
+    "abbrev": "MI"
+  },
+  {
+    "name": "Minnesota",
+    "sid": "27",
+    "abbrev": "MN"
+  },
+  {
+    "name": "Mississippi",
+    "sid": "28",
+    "abbrev": "MS"
+  },
+  {
+    "name": "Missouri",
+    "sid": "29",
+    "abbrev": "MO"
+  },
+  {
+    "name": "Montana",
+    "sid": "30",
+    "abbrev": "MT"
+  },
+  {
+    "name": "Nebraska",
+    "sid": "31",
+    "abbrev": "NE"
+  },
+  {
+    "name": "Nevada",
+    "sid": "32",
+    "abbrev": "NV"
+  },
+  {
+    "name": "New Hampshire",
+    "sid": "33",
+    "abbrev": "NH"
+  },
+  {
+    "name": "New Jersey",
+    "sid": "34",
+    "abbrev": "NJ"
+  },
+  {
+    "name": "New Mexico",
+    "sid": "35",
+    "abbrev": "NM"
+  },
+  {
+    "name": "New York",
+    "sid": "36",
+    "abbrev": "NY"
+  },
+  {
+    "name": "North Carolina",
+    "sid": "37",
+    "abbrev": "NC"
+  },
+  {
+    "name": "North Dakota",
+    "sid": "38",
+    "abbrev": "ND"
+  },
+  {
+    "name": "Ohio",
+    "sid": "39",
+    "abbrev": "OH"
+  },
+  {
+    "name": "Oklahoma",
+    "sid": "40",
+    "abbrev": "OK"
+  },
+  {
+    "name": "Oregon",
+    "sid": "41",
+    "abbrev": "OR"
+  },
+  {
+    "name": "Pennsylvania",
+    "sid": "42",
+    "abbrev": "PA"
+  },
+  {
+    "name": "Rhode Island",
+    "sid": "44",
+    "abbrev": "RI"
+  },
+  {
+    "name": "South Carolina",
+    "sid": "45",
+    "abbrev": "SC"
+  },
+  {
+    "name": "South Dakota",
+    "sid": "46",
+    "abbrev": "SD"
+  },
+  {
+    "name": "Tennessee",
+    "sid": "47",
+    "abbrev": "TN"
+  },
+  {
+    "name": "Texas",
+    "sid": "48",
+    "abbrev": "TX"
+  },
+  {
+    "name": "Utah",
+    "sid": "49",
+    "abbrev": "UT"
+  },
+  {
+    "name": "Vermont",
+    "sid": "50",
+    "abbrev": "VT"
+  },
+  {
+    "name": "Virginia",
+    "sid": "51",
+    "abbrev": "VA"
+  },
+  {
+    "name": "Washington",
+    "sid": "53",
+    "abbrev": "WA"
+  },
+  {
+    "name": "West Virginia",
+    "sid": "54",
+    "abbrev": "WV"
+  },
+  {
+    "name": "Wisconsin",
+    "sid": "55",
+    "abbrev": "WI"
+  },
+  {
+    "name": "Wyoming",
+    "sid": "56",
+    "abbrev": "WY"
+  }
+]

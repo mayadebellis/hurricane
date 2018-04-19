fe417cfc0d9fab6bff6d398988deae5f96f2b2c2
Ballard Blair <glenn.blair92@gmail.com>
2018-04-18 20:19:02 -0400

    got brushing/linking working for pie charts

--- js/drawMap.js
+++ js/drawMap.js
@@ -3,9 +3,23 @@ console.log(root);
 var dataByState = bucketByState(root);
 var currDataSet = dataByState;
 var selectedStates = [];
-
+var clicked1 = false;
 var startYear = 1851;
 var endYear = 2016;
+
+// pie chart
+var width = 260;
+var text = "";
+var height = 200;
+var data1 = [
+  {name: "Category 1", value: 0},
+  {name: "Category 2", value: 0},
+  {name: "Category 3", value: 0},
+  {name: "Category 4", value: 0},
+  {name: "Category 5", value: 0},
+];
+var color1 = d3.scaleOrdinal(d3.schemeCategory10);
+var radius = 50;
 
 var svg = d3.select("svg");
 
@@ -38,22 +52,14 @@ $(".map").not("path").on("click", function (e) {
   if (e.target.nodeName != "path") {
     d3.selectAll(".state")
       .style("opacity", 1);
+
+    d3.selectAll(".pie").remove();
+    selectedStates = [];
   }
+
 });
 
-// pie chart
-var width = 230;
-var text = "";
-var height = 200;
-var data1 = [
-  {name: "Category 1", value: 0},
-  {name: "Category 2", value: 0},
-  {name: "Category 3", value: 0},
-  {name: "Category 4", value: 0},
-  {name: "Category 5", value: 0},
-];
-var color1 = d3.scaleOrdinal(d3.schemeCategory10);
-var radius = 50;
+
 
 var div = d3.select("body").append("div") 
     .attr("id", "tooltip")       
@@ -93,134 +99,7 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
           .filter(function(elem) {return elem.id == d.id;})
           .style("opacity", 1);
 
-        for (var i = 0; state_obj.length > i; i++){
-          if (state_obj[i].category == 1){
-            data1[0].value += 1;
-
-          } else if (state_obj[i].category == 2){
-            data1[1].value += 1;
-
-          } else if (state_obj[i].category == 3){
-            data1[2].value += 1;
-
-          } else if (state_obj[i].category == 4){
-            data1[3].value += 1;
-
-          } else if (state_obj[i].category == 5){
-            data1[4].value += 1;
-
-          }
-        }
-        //console.log(data1);
-        // appends svg to chart area 
-        var pie_svg = d3.select(".chart").append("svg")
-            .attr("class", "pie")
-            .style("width", width)
-            .style("height", height);
-            
-        // TODO: Add title to Pie Chart    
-        // pie_svg.append("text")
-        //     .attr("x", width / 2 )
-        //     .attr("y", 0)
-        //     .style("text-anchor", "middle")
-        //     .text("Title of Diagram");
-
-      
-        var g_pie = pie_svg.append('g')
-        .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
-
-
-        var arc = d3.arc()
-          .innerRadius(0)
-          .outerRadius(radius);
-
-        var label = d3.arc()
-            .outerRadius(radius - 40)
-            .innerRadius(radius - 40);
-
-        var pie = d3.pie()
-          .value(function(d) {return d.value; })
-          .sort(null);
-
-        
-        
-        var g_pie_slice = g_pie.selectAll('g')
-          .data(pie(data1))
-          .enter()
-          .append("g"); 
-
-          g_pie_slice.append("path")
-            .attr('d', arc)
-            .attr('fill', (d,i) => color1(i))
-            .style('opacity', 1)
-            .style('stroke', 'white');
-
-          // g_pie_slice.append("text")
-          //   .attr("class", "name-text")
-          //   .attr('text-anchor', 'middle')
-          //   .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-          //   .attr("dy", ".35em")
-          //   .attr("fill", "black")
-            
-          // .text(function(d){
-          //   //console.log(d.data.name);
-          //   return d.data.name;
-          // });
-        
-
-
-// Chart labels - still under construction 
-  g_pie_slice.append("text")
-      .attr("class", "name-text")
-      .attr("transform", function(d,i){
-      var pos = arc.centroid(d);
-      pos[0] = radius * (midAngle(d) < Math.PI ? 1.1 : -1.1);
-        
-    
-  var percent = (d.endAngle - d.startAngle)/(2*Math.PI)*100
-       if(percent<3){
-       //console.log(percent)
-       pos[1] += i*15
-       }
-        return "translate("+ pos +")";
-      })
-      .text(function(d) { return d.data.name; })
-      .attr("fill", function(d,i) { return color1(i); })
-      .attr("text-anchor", 'left')
-      .attr("dx", function(d){
-      var ac = midAngle(d) < Math.PI ? 0:-50
-              return ac
-      })
-      .attr("dy", 5 )
-      
-      
-     function midAngle(d) {
-      return d.startAngle + (d.endAngle - d.startAngle) / 2;
-    }
-
-    var polyline = g_pie.selectAll("polyline")
-      .data(pie(data1), function(d) {
-        return d.data.currency;
-      })
-      .enter()
-      .append("polyline")
-      .attr("points", function(d,i) {
-        var pos = arc.centroid(d);
-            pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-         var o=   arc.centroid(d)
-     var percent = (d.endAngle -d.startAngle)/(2*Math.PI)*100
-           if(percent<3){
-           //console.log(percent)
-           o[1] 
-           pos[1] += i*15
-           }
-           //return [label.centroid(d),[o[0],0[1]] , pos];
-            return [label.centroid(d),[o[0],pos[1]] , pos];
-          })
-          .style("fill", "none")
-          //.attr('stroke','grey')
-          .attr("stroke", function(d,i) { return color1(i); })
-          .style("stroke-width", "1px");
+    // drawPie(d);
 
   })
       .on("mouseout", function(d) {  
@@ -247,18 +126,23 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
           data1[4].value = 0;
           
           //Clears old pie chart when not hovering
-          var old_pie = d3.selectAll(".pie").remove(); 
+          // var old_pie = d3.selectAll(".pie").remove(); 
       })
 
       .on("click", function(d) {
         // brushing
         if (!selectedStates.includes(d.id)) {
           selectedStates.push(d.id);
+          drawPie(d);
+          console.log("1", selectedStates);
         } else {
           var i = selectedStates.indexOf(d.id);
           if (i > -1) {
             selectedStates.splice(i, 1);
           }
+          var id = "#pie" + d.id;
+          console.log(id);
+          d3.selectAll(id).remove();
         }
 
         d3.selectAll(".state")
@@ -268,12 +152,12 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
           .filter(function(elem) {return selectedStates.includes(elem.id);})
           .style("opacity", 1);
 
-        console.log(selectedStates)
+        console.log(selectedStates);        
         });
 
-        //TODO @Ballard - when you click two states both pie charts will show up
-
 });
+
+
 
 function showToolTip(d, dataSet) {
   div.transition()    
@@ -443,6 +327,149 @@ function bucketByState(root) {
     }
     ////console.log(stateData);
     return stateData;
+}
+
+function drawPie (d){
+  var state_obj = (currDataSet.find(function(element) {return element.sid == d.id;})).hurricanes;
+  for (var i = 0; state_obj.length > i; i++){
+          if (state_obj[i].category == 1){
+            data1[0].value += 1;
+
+          } else if (state_obj[i].category == 2){
+            data1[1].value += 1;
+
+          } else if (state_obj[i].category == 3){
+            data1[2].value += 1;
+
+          } else if (state_obj[i].category == 4){
+            data1[3].value += 1;
+
+          } else if (state_obj[i].category == 5){
+            data1[4].value += 1;
+
+          }
+        }
+        //console.log(data1);
+        // appends svg to chart area 
+        var pie_svg = d3.select(".chart").append("svg")
+            .attr("class", "pie")
+            .attr("id", ("pie" + d.id))
+            .style("width", width)
+            .style("height", height);
+            
+        // TODO: Add title to Pie Chart    
+        // pie_svg.append("text")
+        //     .attr("x", width / 2 )
+        //     .attr("y", 0)
+        //     .style("text-anchor", "middle")
+        //     .text("Title of Diagram");
+
+      
+        var g_pie = pie_svg.append('g')
+        .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
+
+
+        var arc = d3.arc()
+          .innerRadius(0)
+          .outerRadius(radius);
+
+        var label = d3.arc()
+            .outerRadius(radius - 40)
+            .innerRadius(radius - 40);
+
+        var pie = d3.pie()
+          .value(function(d) {return d.value; })
+          .sort(null);
+
+        
+        
+        var g_pie_slice = g_pie.selectAll('g')
+          .data(pie(data1))
+          .enter()
+          .append("g"); 
+
+          g_pie_slice.append("path")
+            .attr('d', arc)
+            .attr('fill', (d,i) => color1(i))
+            .style('opacity', 1)
+            .style('stroke', 'white');
+
+          // g_pie_slice.append("text")
+          //   .attr("class", "name-text")
+          //   .attr('text-anchor', 'middle')
+          //   .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+          //   .attr("dy", ".35em")
+          //   .attr("fill", "black")
+            
+          // .text(function(d){
+          //   //console.log(d.data.name);
+          //   return d.data.name;
+          // });
+        
+
+
+// Chart labels - still under construction 
+  g_pie_slice.append("text")
+      .attr("class", "name-text")
+      .attr("transform", function(d,i){
+      var pos = arc.centroid(d);
+      pos[0] = radius * (midAngle(d) < Math.PI ? 1.2 : -1.2);
+        
+    
+  var percent = (d.endAngle - d.startAngle)/(2*Math.PI)*100
+       if(percent<3){
+       //console.log(percent)
+       pos[1] += i*15
+       }
+        return "translate("+ pos +")";
+      })
+      .text(function(d) { 
+        if (d.value > 0) {
+          return d.data.name;
+        }
+         })
+      .attr("fill", function(d,i) { return color1(i); })
+      .attr("text-anchor", 'left')
+      .attr("dx", function(d){
+      var ac = midAngle(d) < Math.PI ? 0:-50
+              return ac
+      })
+      .attr("dy", 5 )
+      
+      
+     function midAngle(d) {
+      return d.startAngle + (d.endAngle - d.startAngle) / 2;
+    }
+
+    var polyline = g_pie.selectAll("polyline")
+      .data(pie(data1), function(d) {
+        return d.data.currency;
+      })
+      .enter()
+      .append("polyline")
+      .attr("points", function(d,i) {
+        var pos = arc.centroid(d);
+            pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+         var o=   arc.centroid(d)
+         var percent = (d.endAngle -d.startAngle)/(2*Math.PI)*100
+           if(percent<3){
+           //console.log(percent)
+           o[1] 
+           pos[1] += i*15
+           }
+           //return [label.centroid(d),[o[0],0[1]] , pos];
+            return [label.centroid(d),[o[0],pos[1]] , pos];
+          })
+          .style("fill", "none")
+          //.attr('stroke','grey')
+          .attr("stroke", function(d,i) { return color1(i); })
+          .style("stroke-width", function(d,i){
+            if (d.value > 0){
+              return "1px";
+            } else {
+              return "0px";
+            }
+          });
 }
 
 
